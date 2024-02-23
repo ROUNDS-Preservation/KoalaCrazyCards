6bd13e909272292c92dd6244a315a9402437ac44
Autuwumn <112526071+Autuwumn@users.noreply.github.com>
2024-02-23 01:53:48 -0700

    Add files via upload

---
+++ Cards/WhyAmITheMaster.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnboundLib.Cards;
+using UnityEngine;
+using KCC;
+using UnboundLib.Extensions;
+using Photon.Pun;
+
+namespace KCC.Cards
+{
+    internal class WhyAmITheMaster : CustomCard
+    {
+
+        internal static CardInfo card = null;
+
+        public override string GetModName()
+        {
+            return KCC.ModInitials;
+        }
+        public override TableRefHelper GetTranslationData()
+        {
+            return new TableRefHelper().
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.en_US, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.fr, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.it, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.de, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.es, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.pt_BR, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.ru, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.ja, "Why am i the master?").
+                UpdateSrting("KoalaCrazyCard1", TableRefHelper.Locales.zh, "Why am i the master?");
+        }
+        
+        protected override string GetTitle()
+        {
+            return "KoalaCrazyCard1";
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return KCC.Assets.LoadAsset<GameObject>("C_PeakArt");
+        }
+
+        protected override string GetDescription()
+        {
+            return "<color=red>Command Line: ret: 1 :</color>";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new[]
+            {
+                new CardInfoStat
+                {
+                    amount = "Some",
+                    positive = true,
+                    simepleAmount = CardInfoStat.SimpleAmount.Some,
+                    stat = "Hurtness"
+                },
+                new CardInfoStat
+                {
+                    amount = "Loads of",
+                    positive = true,
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotOf,
+                    stat = "Shooting Rechargement"
+                },
+                new CardInfoStat
+                {
+                    amount = "A massive amount of",
+                    positive = true,
+                    simepleAmount = CardInfoStat.SimpleAmount.aHugeAmountOf,
+                    stat = "Munitions"
+                }
+            };
+        }
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            gun.damage = 2;
+            gun.attackSpeed = 1f / 6f;
+            gun.ammo = 69;
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.MagicPink;
+        }
+    }
+}
---
+++ KoalaCrazyCards.cs
@@ -0,0 +1,81 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnboundLib.Cards;
+using Photon.Pun;
+using Jotunn.Utils;
+using UnityEngine;
+using BepInEx.Configuration;
+using UnboundLib.Utils.UI;
+using System.Collections.Generic;
+using System.Linq;
+using UnboundLib;
+using System.Net.NetworkInformation;
+using UnboundLib.GameModes;
+using System.Collections;
+using System;
+using UnboundLib.Utils;
+using Photon.Realtime;
+using System.Reflection;
+using System.Data;
+using KCC.Cards;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
+using Steamworks;
+using static System.Net.WebRequestMethods;
+
+namespace KCC
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    internal class KCC : BaseUnityPlugin
+    {
+        private const string ModId = "koala.rounds.koalascrazycards";
+        private const string ModName = "Koalas Crazy Cards";
+        private const string Version = "0.0.0";
+        public const string ModInitials = "KCC";
+
+        public static KCC instance;
+
+        internal static AssetBundle Assets;
+
+
+        private void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+
+            if (!instance) instance = this; else Destroy(this);
+
+            Assets = AssetUtils.LoadAssetBundleFromResources("kccassets", typeof(KCC).Assembly);
+        }
+        private void Start()
+        {
+            CustomCard.BuildCard<WhyAmITheMaster>((card) => { WhyAmITheMaster.card = card; });
+
+
+
+            //if (SteamUser.GetSteamID().m_SteamID == 76561198886618670)
+            /*{
+                Destroy(GameObject.Find("UNUSED").transform.GetChild(1).gameObject.GetComponent<ParticleSystem>());
+                Instantiate(Assets.LoadAsset<GameObject>("KoalasFireParticles").GetComponent<ParticleSystem>(), GameObject.Find("UNUSED").transform.GetChild(1));
+            }*/
+        }
+        public void FixedUpdate()
+        {
+            //if (SteamUser.GetSteamID().m_SteamID == 76561198886618670)
+            /*{
+                GameObject.Find("UNUSED").transform.GetChild(3).gameObject.SetActive(true);
+                GameObject.Find("UNUSED").transform.GetChild(1).gameObject.SetActive(true);
+                for (int i = 0; i < GameObject.Find("FrontParticles").transform.childCount; i++)
+                {
+                    var obj = GameObject.Find("FrontParticles").transform.GetChild(i);
+                    if (i != 9) obj.gameObject.SetActive(false);
+                }
+                GameObject.Find("LightCamera").GetComponent<SFRenderer>().shadows = false;
+            }*/
+        }
+        
+    }
+}
